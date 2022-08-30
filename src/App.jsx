511f5aff667d26e584f26038b8299eb4ec348201
Anthony Lukach <anthonylukach@gmail.com>
2022-08-29 22:40:15 -0700

    Bugfix for initial params

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ import { ResultsTable } from "./ResultsTable";
 import { ResultsMap } from "./ResultsMap";
 
 export function App() {
-  const [params, setParams] = useState();
+  const [params, setParams] = useState({
+    dbUrl: process.env.STAC_URL,
+    stacUrl: process.env.DB_URL,
+  });
   const [query, setQuery] = useState();
   const [records, message] = useQuery(query, params.dbUrl);
 
   return (
     <div className="container">
-      <DbConnectionDetails onSubmit={setParams} />
+      <DbConnectionDetails onSubmit={setParams} initialParams={params} />
       <QueryInput onSubmit={setQuery} className="my-2" />
       <code>{message}</code>
       {records && records.length && records[0].hasOwnProperty("geometry") && (
